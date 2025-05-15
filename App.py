0dfed2d376db96e86aaeadb461ad78cab040a05f
nguyentron2909 <61543440+nguyentron2909@users.noreply.github.com>
2025-05-15 16:22:02 +0700

    Update App.py
    added 1 more button

--- App.py
+++ App.py
@@ -4,10 +4,11 @@ from time import perf_counter
 from turtle import RawTurtle, ScrolledCanvas, TurtleScreen
 from map_utils import Map
 from pathfinding import A_Star
+
 class App:
     def __init__(self, root):
         self.root = root
-        root.title("A* Pathfinding")
+        root.title("Pathfinding Demo")
         
         # Left: Canvas chứa turtle
         self.canvas_frame = tk.Frame(root)
@@ -16,29 +17,35 @@ class App:
         self.cv.pack(fill=tk.BOTH, expand=True)
         self.screen = TurtleScreen(self.cv)      # <-- từ turtle, không phải tk
         self.screen.mode("world")
-        self.screen.setworldcoordinates(-410, -710, 410, 710)
+        self.screen.setworldcoordinates(-410, -710, 410, 710)     
         self.t = RawTurtle(self.screen, shape='turtle')
 
         
         # Right: Controls & Legend
         ctrl = tk.Frame(root, padx=10, pady=10)
         ctrl.pack(side=tk.RIGHT, fill=tk.Y)
-        
         # Start button
-        self.btn_start = tk.Button(ctrl, text="Bắt đầu giải mã", command=self.start)
-        self.btn_start.pack(fill=tk.X, pady=5)
         
+
         # Legend
         tk.Label(ctrl, text="Chú thích:", font=('Arial', 12, 'bold')).pack(anchor='w', pady=(20,5))
         self._add_legend(ctrl, "Đích đến", 'orange')
         self._add_legend(ctrl, "Vật cản / Tường", 'black')
         self._add_legend(ctrl, "Đường đi", 'blue')
-        
+       
+        self.btn_start = tk.Button(ctrl, text="Giải bằng A*", command=self.start_a_star)
+        self.btn_start.pack(fill=tk.X, pady=5)
+
+        self.btn_start2 = tk.Button(ctrl, text="Giải bằng cái mẹ j đấy", command=self.start_what_ever)
+        self.btn_start2.pack(fill=tk.X, pady=5)
+
         # Info & finish/reset buttons (ẩn ban đầu)
         self.info_label = tk.Label(ctrl, text="", wraplength=200, justify='left')
         self.btn_finish = tk.Button(ctrl, text="Hoàn tất", command=root.quit)
         self.btn_reset  = tk.Button(ctrl, text="Quay lại", command=self.reset)
         
+        
+
         # Prepare map & turtle
         self.width_half, self.height_half = 400, 700
         self._prepare_map()
@@ -59,8 +66,8 @@ class App:
         tk.Label(f, text=label).pack(side=tk.RIGHT, padx=5)
     
     def _prepare_map(self):
-        self.btn_start.config(state=tk.DISABLED)
         # khởi tạo bản đồ, start/goal
+        
         self.mmap = Map.map_init(10,10)
         self.goals = Map.map_random(self.mmap)   # list of 3 goals
         self.start = Map.random_curpos(self.mmap)
@@ -70,9 +77,8 @@ class App:
         # place turtle at start
         sx, sy = Map.turn2pixel(self.mmap, self.height_half, self.width_half, *self.start)
         self.t.up(); self.t.goto(sx, sy); self.t.down()
-        self.btn_start.config(state=tk.NOMAL)
     
-    def start(self):
+    def start_a_star(self):
         self.btn_start.config(state=tk.DISABLED)
         t0 = perf_counter()
         
@@ -91,7 +97,23 @@ class App:
         self.info_label.pack(pady=(20,5))
         self.btn_finish.pack(fill=tk.X, pady=2)
         self.btn_reset.pack(fill=tk.X, pady=2)
-    
+        self.btn_start.config(state=tk.DISABLED)
+
+    def start_what_ever(self):
+        self.btn_start2.config(state=tk.DISABLED)
+        t0 = perf_counter()
+        
+        # chạy A*
+        complete_run = 1 #what ever ur algorithm is 
+        
+        dt = perf_counter() - t0
+        if complete_run:
+            info_text = f"Giải mã thành công!\nThời gian giải mã: {dt:.3f} giây"
+            self.info_label.config(text=info_text, fg="green")
+        else:
+            info_text = f"Giải mã thất bại!\nThời gian giải mã: {dt:.3f} giây"
+            self.info_label.config(text=info_text, fg="red")
+
     def reset(self):
         # ẩn info & nút finish/reset, hiện lại start
         self.info_label.pack_forget()
